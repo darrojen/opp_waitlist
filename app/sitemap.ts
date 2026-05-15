15240d57405e5078f169d9fe685460a9cd72d26b
darrojen <darlington.ajaezo@gmail.com>
2026-05-15 22:01:26 +0100

    chore: google change

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://opphex.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
